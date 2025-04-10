92825f2ca61ac54b94de10c8acb189dbab452600
abdiaziz77 <abdiaziznoor20@gmail.com>
2025-04-10 23:39:57 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
+import { username, city } from '../data/user';
 
-function Home() {
+const Home = () => {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+      <h1>{username}</h1>
+      <p>{city}</p>
     </div>
   );
 }
+
+export default Home;
